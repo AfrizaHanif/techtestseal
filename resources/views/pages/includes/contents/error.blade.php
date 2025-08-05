cd14e3502af5874e531c44e331656828260cc138
AfrizaHanif <afrizahanif728@gmail.com>
2025-08-05 07:10:00 +0700

    (250805) Bug Fix - Fixed Error Title

--- resources/views/pages/includes/contents/error.blade.php
+++ resources/views/pages/includes/contents/error.blade.php
@@ -2,7 +2,7 @@
 <div class="container my-5">
     <div class="row p-4 pb-0 pe-lg-0 pt-lg-5 align-items-center rounded-3 border shadow-lg bg-danger text-white">
         <div class="col-lg-7 p-3 p-lg-5 pt-lg-3">
-            <h1 class="display-4 fw-bold lh-1 text-white">Koneksi Error</h1>
+            <h1 class="display-4 fw-bold lh-1 text-white">{{ $title_error }}</h1>
             <p class="lead">{{ $error }}. Silahkan refresh halaman / situs ini beberapa saat lagi.</p>
             <div class="d-grid gap-2 d-md-flex justify-content-md-start mb-4 mb-lg-3">
                 <a href="{{ Request::url() }}" type="button" class="btn btn-light btn-lg px-4 me-md-2 fw-bold show-preloader">
